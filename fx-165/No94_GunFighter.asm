1948c8667fb3af73f9d018fda733bc553632d8b0
Shibazaki Yuhei <shibazaki.yuhei@fujitsu.com>
2022-02-23 18:24:25 +0900

    Add GunFighter source.

---
+++ fx-165/No94_GunFighter.asm
@@ -0,0 +1,47 @@
+	org	0x00
+	ldi	0xf
+	scall	0xc		;1.6秒WAIT
+waiting:
+	abyz			;Aレジスタ値[f]をBに退避
+flyingKeyDown:
+	ink	
+	jmpf	gameStart	;ゲーム開始前に入力無し
+	jmpf	judgeFlying
+gameStart:
+	ldi	3
+	scall	0xc		;0.4秒WAIT(0.4sec×16回=6.4secとなる)
+	abyz			;Bに退避した[f]をAに戻す
+	addi	0xf		;fにキャリーが出なくなるまでf足す(16回稼げる)
+	jmpf	waiting		;キャリー発生中は戻って待ち継続(1.6+6.4で計8秒のWAITタイム)
+	ldyi	3		;センターLED点灯
+	scall	1
+	ldyi	6		;[4]勝利時のLED位置を格納
+keyWait:			;入力判定
+	ink	
+	jmpf	keyWait
+	cpi	4
+	jmpf	win5orOther
+	jmpf	winLedOn	;[4]勝利の場合
+win5orOther:
+	cpi	7
+	jmpf	keyWait		;入力値が[7]でない場合入力待ちに戻る
+	ldyi	0		;[7]勝利の場合[7]勝利時のLED位置を格納
+winLedOn:
+	scall	1		;勝利用LEDをON
+	scall	7		;勝負あった音
+end1:				;処理終了
+	jmpf	end1
+judgeFlying:
+	ldyi	5		;[4]フライング時のLED位置を格納
+	cpi	4
+	jmpf	flying7orNoflying
+	jmpf	flyingLedOn	;[4]フライングの場合
+flying7orNoflying:
+	cpi	7
+	jmpf	flyingKeyDown	;入力値が[7]でない場合入力待ちに戻る
+	ldyi	1		;[7]フライングの場合[7フライング時のLED位置を格納
+flyingLedOn:
+	scall	1		;フライング用LEDをON
+	scall	8		;フライングエラー音
+end2:				;処理終了
+	jmpf	end2
