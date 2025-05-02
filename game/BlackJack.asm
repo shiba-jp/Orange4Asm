8e0bbb7312e89545fcb06f7adf44aa7c3b08e9bb
shiba-jp <shibazaki.yuhei@gmail.com>
2025-05-02 16:10:35 +0900

    mod blackJack.(add comment)

--- game/BlackJack.asm
+++ game/BlackJack.asm
@@ -2,14 +2,22 @@
 ;BlackJack
 ;----------------------------------------
 ;[How to play]
+;・長い音の後、0以外のボタンでカード引く
+;・1回目のカードを引いた後、0を押すと確定、0以外で次のカードルーレットが回るので任意のボタンで引く
+;・数字LEDには初回の値と、2回目以降の足された値を表示
+;・通常LEDには、2回目以降に引いた値を2進数で表示（点灯）
+;・引いた値がFになった場合はその場でBlackJack
+;・引いた値がFを超えた場合はブレイク（数字LEDに桁上がりの点が点灯する）
 ;
 ;[Description]
 ;
 ;[Memory]
+;50:引いたカードの値
+;5E:2進数LEDの表示用
 ;
 ;[Machine Code]
 ;E00:80A04AF4EAF6080A050F12C0F20E7F1DEAF6080F12
-;E80:AfB10F82E93AE4EDA06FAA14CFFA0F1B0FA7FA0F611A7E1E8F1D
+;E80:AFB10F82E93AE4EDA06FAA14CFFA0F1B0FA7FA0F611A7E1E8F1D
 ;----------------------------------------
 	org	0x00
 init	ldi	0
@@ -24,16 +32,16 @@ first	scall	A
 confirm	ink	
 	jmpf	confirm
 	cpi	0
-	jmpf	add
+	jmpf	addVal
 blackjack	scall	7
 end	jmpf	end
-add	scall	A
+addVal	scall	A
 	call	random
 	jmpf	confirm
 
 
 	org	0x80
-random	ldyi	f
+random	ldyi	F
 addNo	addyi	1
 	ink	
 	jmpf	addNo
